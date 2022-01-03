48eb66f5cd507cde58054cf36d1d734d2cd7b73a
BrunoHams <bruno.hams@hotmail.com>
2022-01-03 19:13:29 -0300

    [ADD] Display Images with Coil

--- app/src/main/java/com/codingwithmitch/dotainfo/ui/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/dotainfo/ui/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import coil.ImageLoader
 import com.codingwithmitch.core.DataState
 import com.codingwithmitch.core.Logger
 import com.codingwithmitch.core.ProgressBarState
 import com.codingwithmitch.core.UIComponent
+import com.codingwithmitch.dotainfo.R
 import com.codingwithmitch.dotainfo.ui.theme.DotaInfoTheme
 import com.codingwithmitch.hero_interactors.HeroInteractors
 import com.codingwithmitch.ui_herolist.ui.HeroList
@@ -23,9 +25,17 @@ class MainActivity : ComponentActivity() {
 
     private val state: MutableState<HeroListState> = mutableStateOf(HeroListState())
     private val progressBarState: MutableState<ProgressBarState> = mutableStateOf(ProgressBarState.Idle)
+    private lateinit var imageLoader: ImageLoader
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        imageLoader = ImageLoader.Builder(applicationContext)
+            .error(R.drawable.error_image)
+            .placeholder(R.drawable.white_background)
+            .availableMemoryPercentage(.25)
+            .crossfade(true)
+            .build()
 
         val getHeroes = HeroInteractors.build(
             sqlDriver = AndroidSqliteDriver(
@@ -59,7 +69,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             DotaInfoTheme {
                 HeroList(
-                    state = state.value
+                    state = state.value,
+                    imageLoader = imageLoader
                 )
             }
         }
--- hero/ui-heroList/src/main/java/com/codingwithmitch/ui_herolist/ui/HeroList.kt
+++ hero/ui-heroList/src/main/java/com/codingwithmitch/ui_herolist/ui/HeroList.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import coil.ImageLoader
 import com.codingwithmitch.core.ProgressBarState
 import com.codingwithmitch.ui_herolist.HeroListState
 import com.codingwithmitch.ui_herolist.components.HeroListItem
@@ -15,12 +16,14 @@ import com.codingwithmitch.ui_herolist.components.HeroListItem
 @Composable
 fun HeroList(
     state: HeroListState,
+    imageLoader: ImageLoader
 ) {
     Box(modifier = Modifier.fillMaxSize()) {
         LazyColumn{
             items(state.heroes) { hero ->
                 HeroListItem(
                     hero = hero,
+                    imageLoader = imageLoader,
                     onSelectHero = {}
                 )
             }
